3f8fdfa8a1f1935314e61e7bf1e552257074aa11
Tobias Fuchs <fuchs.tobias.ext@siemens.com>
2015-02-02 16:57:22 +0100

    containers_cpp: reapplied consistent naming of test cases

--- containers_cpp/test/main.cc
+++ containers_cpp/test/main.cc
@@ -24,6 +24,14 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include <embb/containers/lock_free_tree_value_pool.h>
+#include <embb/containers/wait_free_array_value_pool.h>
+#include <embb/containers/wait_free_spsc_queue.h>
+#include <embb/containers/object_pool.h>
+#include <embb/containers/lock_free_stack.h>
+#include <embb/containers/lock_free_mpmc_queue.h>
+#include <embb/base/c/memory_allocation.h>
+
 #include <partest/partest.h>
 #include <embb/base/thread.h>
 
@@ -32,44 +40,36 @@
 #include "./stack_test.h"
 #include "./hazard_pointer_test.h"
 #include "./object_pool_test.h"
-#include <embb/containers/lock_free_tree_value_pool.h>
-#include <embb/containers/wait_free_array_value_pool.h>
-#include <embb/containers/wait_free_spsc_queue.h>
-#include <embb/containers/object_pool.h>
-#include <embb/containers/lock_free_stack.h>
-#include <embb/containers/lock_free_mpmc_queue.h>
-
-#include <embb/base/c/memory_allocation.h>
 
 #define COMMA ,
 
+using embb::containers::WaitFreeArrayValuePool;
+using embb::containers::LockFreeTreeValuePool;
+using embb::containers::WaitFreeSPSCQueue;
+using embb::containers::LockFreeMPMCQueue;
+using embb::containers::LockFreeStack;
+using embb::containers::LockFreeTreeValuePool;
+using embb::containers::WaitFreeArrayValuePool;
+using embb::containers::test::PoolTest;
+using embb::containers::test::HazardPointerTest;
+using embb::containers::test::QueueTest;
+using embb::containers::test::StackTest;
+using embb::containers::test::ObjectPoolTest;
+
 PT_MAIN("Data Structures C++") {
-  unsigned int max_threads =
-      static_cast<unsigned int>(2 * partest::TestSuite::GetDefaultNumThreads());
+  unsigned int max_threads = static_cast<unsigned int>(
+    2 * partest::TestSuite::GetDefaultNumThreads());
   embb_thread_set_max_count(max_threads);
 
-  PT_RUN(embb::containers::test::PoolTest<
-    embb::containers::WaitFreeArrayValuePool<int COMMA -1> >);
-  PT_RUN(embb::containers::test::PoolTest<
-    embb::containers::LockFreeTreeValuePool<int COMMA -1> >);
-
-  PT_RUN(embb::containers::test::HazardPointerTest);
-
-  PT_RUN(embb::containers::test::QueueTest<
-    embb::containers::WaitFreeSPSCQueue< ::std::pair<size_t COMMA int> > >);
-
-  PT_RUN(embb::containers::test::QueueTest<
-    embb::containers::LockFreeMPMCQueue< ::std::pair<size_t COMMA int> >
+  PT_RUN(PoolTest< WaitFreeArrayValuePool<int COMMA -1> >);
+  PT_RUN(PoolTest< LockFreeTreeValuePool<int COMMA -1> >);
+  PT_RUN(HazardPointerTest);
+  PT_RUN(QueueTest< WaitFreeSPSCQueue< ::std::pair<size_t COMMA int> > >);
+  PT_RUN(QueueTest< LockFreeMPMCQueue< ::std::pair<size_t COMMA int> >
     COMMA true COMMA true >);
-
-  PT_RUN(embb::containers::test::StackTest<
-    embb::containers::LockFreeStack<int> >);
-
-  PT_RUN(embb::containers::test::ObjectPoolTest
-    <embb::containers::LockFreeTreeValuePool<bool COMMA false > >);
-
-  PT_RUN(embb::containers::test::ObjectPoolTest
-    <embb::containers::WaitFreeArrayValuePool<bool COMMA false> >);
+  PT_RUN(StackTest< LockFreeStack<int> >);
+  PT_RUN(ObjectPoolTest< LockFreeTreeValuePool<bool COMMA false > >);
+  PT_RUN(ObjectPoolTest< WaitFreeArrayValuePool<bool COMMA false> >);
 
   PT_EXPECT(embb_get_bytes_allocated() == 0);
 }
