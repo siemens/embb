4e2b486ec8090d80fc1e2e59ef2c8d9f4c02ec07
Tobias Fuchs <fuchs.tobias.ext@siemens.com>
2015-03-31 18:04:52 +0200

    dataflow_cpp: fix in unit test

--- dataflow_cpp/test/dataflow_cpp_test_simple.cc
+++ dataflow_cpp/test/dataflow_cpp_test_simple.cc
@@ -158,7 +158,7 @@ void SimpleTest::TestBasic() {
     128,  // max groups (default: 128)
     // Currently needs to be initialized
     // with (max_queues + 1), see defect embb449
-    num_cores, // max queues (default: 16)
+    num_cores + 1, // max queues (default: 16)
     1024, // queue capacity (default: 1024)
     4     // num priorities (default: 4)
   );
