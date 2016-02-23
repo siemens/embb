d405a2af9545b273f9ea2b78a08d6ace26308a83
Marcus Winter <marcus.winter.ext@siemens.com>
2016-02-23 14:58:22 +0100

    base_cpp: removed redundant assignment in test

--- base_cpp/test/atomic_test.cc
+++ base_cpp/test/atomic_test.cc
@@ -173,10 +173,6 @@ AtomicTest::TestStressSwap::TestStressSwap(
   size_t number_threads, size_t number_iterations)
   : TestUnit("Swap Stress test for Atomics"), swap1_counter(1)
   , swap2_counter(2) {
-  bitsets[0] = std::bitset<ATOMIC_TESTS_ITERATIONS * 2 + 1>();
-  bitsets[1] = std::bitset<ATOMIC_TESTS_ITERATIONS * 2 + 1>();
-  bitsets[2] = std::bitset<ATOMIC_TESTS_ITERATIONS * 2 + 1>();
-
   PT_ASSERT(number_threads == 1);
   Pre(&TestStressSwap::Init, this);
 
