9bcc7d15d54218f7c6cb87e13af92c350bb1121e
Marcus Winter <marcus.winter.ext@siemens.com>
2016-04-19 08:56:12 +0200

    examples: fixed mtapi_cpp example

--- doc/examples/mtapi/mtapi_cpp_wait_task-snippet.h
+++ doc/examples/mtapi/mtapi_cpp_wait_task-snippet.h
@@ -1,4 +1,4 @@
-    mtapi_status_t status = task.Wait(MTAPI_INFINITE);
+    status = task.Wait(MTAPI_INFINITE);
     if (status != MTAPI_SUCCESS) {
       printf("task failed with error: %d\n\n", status);
       exit(status);
