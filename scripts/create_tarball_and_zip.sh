3eb162a65686e2770229070f30ee08bfd91bb39c
Marcus Winter <marcus.winter.ext@siemens.com>
2016-12-12 09:49:08 +0100

    scripts: path problem again

--- scripts/create_tarball_and_zip.sh
+++ scripts/create_tarball_and_zip.sh
@@ -424,7 +424,7 @@ fi
 #build the zip, if CREATE_ZIP is true
 if [ "$CREATE_ZIP" = true ]; then
 	echo "--> Calling zip"
-        REMEMBER_FULL_CUR_DIR=`realpath $(pwd)`
+        REMEMBER_FULL_CUR_DIR="`realpath $(pwd)`"
 	cd $MYTMPDIR
         zip -r -q $ZIP_NAME ./*
 	mv $ZIP_NAME $REMEMBER_FULL_CUR_DIR
