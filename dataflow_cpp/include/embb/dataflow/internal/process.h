f1963d2aeab25e4d7a334e17cae1f2d0398f5416
Marcus Winter <marcus.winter.ext@siemens.com>
2017-03-15 11:13:37 +0100

    removed coverity annotations

--- dataflow_cpp/include/embb/dataflow/internal/process.h
+++ dataflow_cpp/include/embb/dataflow/internal/process.h
@@ -62,7 +62,6 @@ class Process< Serial, Inputs<I1, I2, I3, I4, I5>,
     queued_clock_ = 0;
     bool ordered = Serial;
     if (ordered) {
-      // coverity[dead_error_line]
       queue_id_ = GetNextProcessID();
     } else {
       queue_id_ = 0;
@@ -80,7 +79,6 @@ class Process< Serial, Inputs<I1, I2, I3, I4, I5>,
     queued_clock_ = 0;
     bool ordered = Serial;
     if (ordered) {
-      // coverity[dead_error_line]
       queue_id_ = GetNextProcessID();
     } else {
       queue_id_ = 0;
