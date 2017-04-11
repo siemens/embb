627b76fda7b9e1f38128d437e7f0ef4762267f14
Marcus Winter <marcus.winter.ext@siemens.com>
2017-04-11 12:31:42 +0200

    algorithms_cpp: added ForLoop

--- algorithms_cpp/include/embb/algorithms/for_each.h
+++ algorithms_cpp/include/embb/algorithms/for_each.h
@@ -29,6 +29,7 @@
 
 #include <embb/mtapi/job.h>
 #include <embb/mtapi/execution_policy.h>
+#include <embb/algorithms/internal/int_iterator.h>
 
 namespace embb {
 namespace algorithms {
@@ -87,6 +88,52 @@ void ForEach(
             available cores. */
   );
 
+/**
+ * Applies a unary function to the integers of a range in parallel.
+ *
+ * The range consists of the integers from \c first to \c last, excluding the
+ * last element, strided by \c stride.
+ *
+ * \throws embb::base::ErrorException if not enough MTAPI tasks can be created
+ *         to satisfy the requirements of the algorithm.
+ * \threadsafe if the integers in the range are not modified by another thread
+ *             while the algorithm is executed.
+ * \note No guarantee is given on the order in which the function is applied to
+ *       the integers.<br/>
+ *       For nested algorithms, the task limit may be exceeded. In that case,
+ *       increase the task limit of the MTAPI node.
+ * \see embb::mtapi::ExecutionPolicy, ZipIterator
+ * \tparam Integer integer type
+ * \tparam Function Unary function with argument of type
+ *         <tt>std::iterator_traits<RAI>::value_type</tt> or an
+ *         embb::mtapi::Job associated with an action function accepting a
+ *         struct containing one member of type
+ *         <tt>std::iterator_traits<RAI>::value_type</tt>
+ *         as its argument buffer and a struct containing one member of type
+ *         <tt>std::iterator_traits<RAI>::value_type</tt>
+ *         as its result buffer.
+ */
+template<typename Integer, typename Diff, typename Function>
+void ForLoop(
+  Integer first,
+  /**< [IN] First integer of the range */
+  Integer last,
+  /**< [IN] Last plus one integer of the range */
+  Diff stride = 1,
+  /**< [IN] Stride between integers, can be omitted */
+  Function unary,
+  /**< [IN] Unary function applied to each element in the range */
+  const embb::mtapi::ExecutionPolicy& policy = embb::mtapi::ExecutionPolicy(),
+  /**< [IN] embb::mtapi::ExecutionPolicy for the foreach loop execution */
+  size_t block_size = 0
+  /**< [IN] Lower bound for partitioning the range of integers into blocks that
+            are treated in parallel. Partitioning of a block stops if its size
+            is less than or equal to \c block_size. The default value 0 means
+            that the minimum block size is determined automatically depending on
+            the number of integers in the range divided by the number of
+            available cores. */
+  );
+
 #else // DOXYGEN
 
 /**
@@ -136,6 +183,75 @@ void ForEach(
   const embb::mtapi::ExecutionPolicy& policy
   ) {
   ForEach(first, last, unary, policy, 0);
+}
+
+/**
+ * Overload of above described Doxygen dummy.
+ */
+template<typename Integer, typename Diff, typename Function>
+void ForLoop(
+  Integer first,
+  Integer last,
+  Diff stride,
+  Function unary,
+  const embb::mtapi::ExecutionPolicy& policy,
+  size_t block_size
+  ) {
+  ForEach(internal::IntIterator<Integer>(first, stride),
+    internal::IntIterator<Integer>(last, stride),
+    unary, policy, block_size);
+}
+
+/**
+ * Overload of above described Doxygen dummy.
+ */
+template<typename Integer, typename Function>
+void ForLoop(
+  Integer first,
+  Integer last,
+  Function unary,
+  const embb::mtapi::ExecutionPolicy& policy,
+  size_t block_size
+  ) {
+  ForLoop(first, last, 1, unary, policy, block_size);
+}
+
+/**
+ * Overload of above described Doxygen dummy with less arguments.
+ */
+template<typename Integer, typename Diff, typename Function>
+void ForLoop(
+  Integer first,
+  Integer last,
+  Diff stride,
+  Function unary
+  ) {
+  ForLoop(first, last, stride, unary, embb::mtapi::ExecutionPolicy(), 0);
+}
+
+/**
+ * Overload of above described Doxygen dummy with less arguments.
+ */
+template<typename Integer, typename Function>
+void ForLoop(
+  Integer first,
+  Integer last,
+  Function unary
+  ) {
+  ForLoop(first, last, unary, embb::mtapi::ExecutionPolicy(), 0);
+}
+
+/**
+ * Overload of above described Doxygen dummy with less arguments.
+ */
+template<typename RAI, typename Function>
+void ForLoop(
+  RAI first,
+  RAI last,
+  Function unary,
+  const embb::mtapi::ExecutionPolicy& policy
+  ) {
+  ForLoop(first, last, unary, policy, 0);
 }
 
 #endif // else DOXYGEN
